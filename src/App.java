cb384134484f7eb63a3b6a06b3ac29fe92a22043
mamadouba <mamadoubo.barry@gmail.com>
2015-02-07 16:52:33 +0100

    second version

--- src/App.java
+++ src/App.java
@@ -7,14 +7,11 @@ import java.util.concurrent.Executors;
 public class App {
 	public static void main(String[]args) throws IOException, InterruptedException{
 		if(args[0].equals("-server")){
-			ServerSocket socket = new ServerSocket(2222);
-			while(true){
-			new Thread(new ServerThread(socket.accept())).start();
-			}
+			new Server().runServer();
 		}
 		if(args[0].equals("-client"))
 		{
-			new Client(2222).run();
+			new Client(Server.listenningPort).runClient();
 		}
 	}
 }
@@ -23,103 +20,59 @@ class Client{
 	private PrintWriter output;
 	private BufferedReader input;
 	private Socket socket;
-	
 	public Client(int port) throws IOException{
-		socket = new Socket();
-		socket.connect(new InetSocketAddress("localhost",2222));
+		socket = new Socket("localhost",port);
 	}
-	public void run() throws IOException, InterruptedException{
+	public void runClient() throws IOException, InterruptedException{
 		input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-		output = new PrintWriter(socket.getOutputStream());
+		output = new PrintWriter(socket.getOutputStream(),true);
 		while(true){
-			try {
-				input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				System.out.println("msg:"+input.readLine());
-				output.write("OK");
-				Thread.sleep(3000);
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-		
-	}
-		/*ExecutorService executor = Executors.newFixedThreadPool(2);
-		Runnable emission = new Runnable(){
-			public void run(){
-				while(true){
-					output.println("Hello");
-				}
-			}
-		};
-		Runnable reception = new Runnable(){
-			public void run(){
-				while(true){
-					try {
-						System.out.print("msg"+input.readLine());
-					} catch (IOException e) {
-						e.printStackTrace();
-					}
-				}
-			}
-		};
-		executor.execute(emission);
-		executor.execute(reception);
-		executor.shutdown();
-		while(!executor.isTerminated()){
+			output.println(System.currentTimeMillis());
 			Thread.sleep(3000);
 		}
+	}	
+}
+class Server{
+	public static final int listenningPort = 1234;
+	public void runServer() throws IOException{
+		ExecutorService executor = Executors.newFixedThreadPool(4);
+		ServerSocket server = new ServerSocket(listenningPort);
+		while(true){
+			Socket newClient = server.accept();
+			Runnable runnable = new ServerThread(newClient);
+			executor.execute(runnable);
+		}
 	}
-	*/
 }
 class ServerThread implements Runnable {
 	private PrintWriter output;
 	private BufferedReader input;
 	private Socket socket;
+	private static int number = 0;
 	public ServerThread(Socket socket){
 		this.socket = socket;
+		number++;
 	}
 	
 	public void run(){
-		
 		try {
+			String user = "user"+number;
+			System.out.println(user+" is connected from the port "+socket.getPort());
 			input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 			output = new PrintWriter(socket.getOutputStream());
 			while(true){
-				input = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-				System.out.print(input.readLine());
-				output.println("Hello"+System.currentTimeMillis());
-				output.flush();
-				
+				System.out.println(user+": "+input.readLine());	
 			}
-			/*ExecutorService executor = Executors.newFixedThreadPool(2);
-			Runnable emission = new Runnable(){
-				public void run(){
-					while(true){
-						output.println("Hello");
-					}
-				}
-			};
-			Runnable reception = new Runnable(){
-				public void run(){
-					while(true){
-						try {
-							System.out.print("msg"+input.readLine());
-						} catch (IOException e) {
-							e.printStackTrace();
-						}
-					}
-				}
-			};
-			executor.execute(emission);
-			executor.execute(reception);
-			executor.shutdown();
-			while(!executor.isTerminated()){
-				Thread.sleep(3000);
-			}
-			*/
 		} catch (IOException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
+			try {
+				socket.close();
+				input.close();
+				output.close();
+			} catch (IOException e1) {
+				e1.printStackTrace();
+			}
+			
 		}
 	}
 }
